864697f6e0d0daa0b537c4d82e472c66ab6c2375
Charlycop <charlycop@free.fr>
2019-10-27 10:30:25 +0100

    exec beta version fini (compilation ok)

---
+++ TME4/EXEC/exec.vhdl
@@ -0,0 +1,151 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity EXec is
+	port(
+	-- Decode interface synchro
+			dec2exe_empty	: in Std_logic;
+			exe_pop			: out Std_logic;
+
+	-- Decode interface operands
+			dec_op1			: in Std_Logic_Vector(31 downto 0); -- first alu input
+			dec_op2			: in Std_Logic_Vector(31 downto 0); -- shifter input
+			dec_exe_dest	: in Std_Logic_Vector(3 downto 0); -- Rd destination
+			dec_exe_wb		: in Std_Logic; -- Rd destination write back
+			dec_flag_wb		: in Std_Logic; -- CSPR modifiy
+
+	-- Decode to mem interface 
+			dec_mem_data	: in Std_Logic_Vector(31 downto 0); -- data to MEM W
+			dec_mem_dest	: in Std_Logic_Vector(3 downto 0); -- Destination MEM R
+			dec_pre_index 	: in Std_logic;
+
+			dec_mem_lw		: in Std_Logic;
+			dec_mem_lb		: in Std_Logic;
+			dec_mem_sw		: in Std_Logic;
+			dec_mem_sb		: in Std_Logic;
+
+	-- Shifter command
+			dec_shift_lsl	: in Std_Logic;
+			dec_shift_lsr	: in Std_Logic;
+			dec_shift_asr	: in Std_Logic;
+			dec_shift_ror	: in Std_Logic;
+			dec_shift_rrx	: in Std_Logic;
+			dec_shift_val	: in Std_Logic_Vector(4 downto 0);
+			dec_cy			: in Std_Logic;
+
+	-- Alu operand selection
+			dec_comp_op1	: in Std_Logic;
+			dec_comp_op2	: in Std_Logic;
+			dec_alu_cy 		: in Std_Logic;
+
+	-- Alu command
+			dec_alu_cmd		: in Std_Logic_Vector(1 downto 0);
+
+	-- Exe bypass to decod
+			exe_res			: out Std_Logic_Vector(31 downto 0);
+
+			exe_c			: out Std_Logic;
+			exe_v			: out Std_Logic;
+			exe_n			: out Std_Logic;
+			exe_z			: out Std_Logic;
+
+			exe_dest		: out Std_Logic_Vector(3 downto 0); -- Rd destination
+			exe_wb			: out Std_Logic; -- Rd destination write back
+			exe_flag_wb		: out Std_Logic; -- CSPR modifiy
+
+	-- Mem interface
+			exe_mem_adr		: out Std_Logic_Vector(31 downto 0); -- Alu res register
+			exe_mem_data	: out Std_Logic_Vector(31 downto 0);
+			exe_mem_dest	: out Std_Logic_Vector(3 downto 0);
+
+			exe_mem_lw		: out Std_Logic;
+			exe_mem_lb		: out Std_Logic;
+			exe_mem_sw		: out Std_Logic;
+			exe_mem_sb		: out Std_Logic;
+
+			exe2mem_empty	: out Std_logic;
+			mem_pop			: in Std_logic;
+
+	-- global interface
+			ck				: in Std_logic;
+			reset_n			: in Std_logic;
+			vdd				: in bit;
+			vss				: in bit);
+end EXec;
+
+----------------------------------------------------------------------
+
+architecture Behavior OF EXec is
+signal c_s, exe_push, exe2mem_full : std_logic;
+signal op2_s, toto, toto2, mem_adr : std_logic_vector(31 downto 0);
+
+
+
+begin
+
+toto <= ((dec_op1(31 downto 1)&(dec_op1(0) XOR dec_comp_op1)) AND (NOT X"00000000"));
+toto2 <= op2_s(31 downto 1)&(dec_comp_op2 XOR op2_s(0));
+
+--  Component instantiation.
+	shifter: entity work.Shifter
+	port map (shift_lsl     => dec_shift_lsl,
+	   	  	  shift_lsr     => dec_shift_lsr,
+	          shift_asr     => dec_shift_asr, 
+	          shift_ror     => dec_shift_ror, 
+	          shift_rrx     => dec_shift_rrx, 
+	          shift_val     => dec_shift_val, 
+	          din           => dec_op2,       
+	          cin           => dec_cy,      
+	   	      cout          => c_s,      
+	    	  dout          => op2_s,
+	    	  vdd           => vdd,
+              vss           => vss);
+
+	alu: entity work.Alu
+	port map (op1           => toto,
+        	  op2           => toto2,
+              cin           => c_s,
+       		  cmd           => dec_alu_cmd,
+        	  res           => exe_res,
+        	  cout          => exe_c,
+        	  z             => exe_z,
+        	  n             => exe_n,
+        	  v             => exe_v,
+	    	  vdd           => vdd,
+              vss           => vss);
+
+	exec2mem : entity work.fifo
+	port map (	    din(71)	 => dec_mem_lw,
+					din(70)	 => dec_mem_lb,
+					din(69)	 => dec_mem_sw,
+					din(68)	 => dec_mem_sb,
+
+					din(67 downto 64) => dec_mem_dest,
+					din(63 downto 32) => dec_mem_data,
+					din(31 downto 0)  => mem_adr,
+
+					dout(71)	 => exe_mem_lw,
+					dout(70)	 => exe_mem_lb,
+					dout(69)	 => exe_mem_sw,
+					dout(68)	 => exe_mem_sb,
+
+					dout(67 downto 64) => exe_mem_dest,
+					dout(63 downto 32) => exe_mem_data,
+					dout(31 downto 0)  => exe_mem_adr,
+
+					push     => exe_push,
+					pop		 => mem_pop,
+
+					empty	 => exe2mem_empty,
+					full	 => exe2mem_full,
+
+					reset_n	 => reset_n,
+					ck		 => ck,
+					vdd		 => vdd,
+					vss		 => vss);
+
+
+
+end Behavior;
+
--- TME4/shifter/shifter.vhdl
+++ TME4/shifter/shifter.vhdl
@@ -36,9 +36,9 @@ decalTampon_s  <= X"FFFFFFFF" WHEN (din(31)='1' AND shift_asr='1') else (others 
 
 -- version sans signal --
 dout <=      (din(31-shiftVal_s downto 0)         & decalTampon_s(shiftVal_s-1 downto 0))  WHEN shift_lsl='1'
-	    else (decalTampon_s(shiftVal_s-1 downto 0)& din(31 downto  shiftVal_s ))           WHEN (shift_lsr='1' OR shift_asr='1')
-	    else (din(shiftVal_s-1 downto 0)          & din(31 downto  shiftVal_s ))           WHEN shift_ror='1'
-	    else (cin & din(31 downto 1))                 									     WHEN shift_rrx='1'
+	    else (decalTampon_s(shiftVal_s-1 downto 0)& din(31 downto  shiftVal_s ))    WHEN (shift_lsr='1' OR shift_asr='1')
+	    else (din(shiftVal_s-1 downto 0)          & din(31 downto  shiftVal_s ))    WHEN shift_ror='1'
+	    else (cin & din(31 downto 1))                 				WHEN shift_rrx='1'
 	    else (others=>'0');
 
 -- VERSION CONVENTIONNELLE --
